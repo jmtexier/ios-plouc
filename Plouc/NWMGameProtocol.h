8aa8768421d979cf121e01ab3b973cbb21a94e8a
Jean-Michel TEXIER <jm.texier@gmail.com>
2014-01-01 19:46:21 +0100

    First version of (poor) IA... but it works :)

---
+++ Plouc/NWMGameProtocol.h
@@ -0,0 +1,13 @@
+//
+//  NWMGameProtocol.h
+//  Plouc
+//
+//  Created by Jean-Michel TEXIER on 01/01/14.
+//  Copyright (c) 2014 Jean-Michel TEXIER. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NWMGameProtocol : NSObject
+
+@end
---
+++ Plouc/NWMGameProtocol.m
@@ -0,0 +1,13 @@
+//
+//  NWMGameProtocol.m
+//  Plouc
+//
+//  Created by Jean-Michel TEXIER on 01/01/14.
+//  Copyright (c) 2014 Jean-Michel TEXIER. All rights reserved.
+//
+
+#import "NWMGameProtocol.h"
+
+@implementation NWMGameProtocol
+
+@end
